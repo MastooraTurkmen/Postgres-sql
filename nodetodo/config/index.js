94be5abac9f2e3486f99210c09e9210c1e04762a
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-08-24 20:09:26 +0430

    delete node app

--- nodetodo/config/index.js
+++
@@ -1,7 +0,0 @@
-const configValues = require('./config.json');
-
-module.exports = {
-    getDdConnectionString: function () {
-        return `mongodb://${configValues.uname}:${configValues.pwd}@localhost:27017/nodetodo`;
-    }
-}
